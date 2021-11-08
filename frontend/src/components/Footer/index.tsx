be065672bc28dd53642f5e182496fb7abc741882
KaioFAnjos <kaio.fa@hotmail.com>
2021-11-08 09:23:17 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/KaioFAnjos" target="_blank" rel="noreferrer">Kaio Felipe</a></p>
+                <p className="text-light"><small><strong>Projeto Spring React</strong><br />
+                    Desenvolvimento feito por: <a href="https://instagram.com/six.kaiow" target="_blank" rel="noreferrer">@six.kaiow</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
