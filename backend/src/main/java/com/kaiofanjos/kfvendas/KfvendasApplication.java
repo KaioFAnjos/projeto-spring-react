bbc95b82b2ef923b894141bb729459791b681409
KaioFAnjos <kaio.fa@hotmail.com>
2021-11-07 21:40:13 -0300

    Project created

---
+++ backend/src/main/java/com/kaiofanjos/kfvendas/KfvendasApplication.java
@@ -0,0 +1,13 @@
+package com.kaiofanjos.kfvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kaiofanjos/kfvendas/KfvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiofanjos.kfvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
