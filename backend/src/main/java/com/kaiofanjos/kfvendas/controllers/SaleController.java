846c871dd47b6cdb46c2e8c11919c28a83da37fa
KaioFAnjos <kaio.fa@hotmail.com>
2021-11-09 22:09:32 -0300

    Pagination

--- backend/src/main/java/com/kaiofanjos/kfvendas/controllers/SaleController.java
+++ backend/src/main/java/com/kaiofanjos/kfvendas/controllers/SaleController.java
@@ -1,26 +1,26 @@
 package com.kaiofanjos.kfvendas.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kaiofanjos.kfvendas.dto.SellerDTO;
-import com.kaiofanjos.kfvendas.service.SellerService;
+import com.kaiofanjos.kfvendas.dto.SaleDTO;
+import com.kaiofanjos.kfvendas.service.SaleService;
 
 @RestController
-@RequestMapping(value = "/sellers")
-public class SellerController {
+@RequestMapping(value = "/sales")
+public class SaleController {
 	
 	@Autowired
-	private SellerService service;
+	private SaleService service;
 	
 	@GetMapping 
-	public ResponseEntity<List<SellerDTO>> findAll(){
-		List<SellerDTO> list = service.findAll();
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
+		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
 	}
 }
---
+++ backend/src/main/java/com/kaiofanjos/kfvendas/dto/SaleDTO.java
@@ -0,0 +1,106 @@
+package com.kaiofanjos.kfvendas.dto;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.kaiofanjos.kfvendas.entities.Sale;
+
+public class SaleDTO {
+	
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	private SellerDTO seller;
+	
+	public SaleDTO() {
+		
+	}
+
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+	
+	public SaleDTO(Sale entity) {
+		id = entity.getId();
+		visited = entity.getVisited();
+		deals = entity.getDeals();
+		amount = entity.getAmount();
+		date = entity.getDate();
+		seller = new SellerDTO(entity.getSeller());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public SellerDTO getSeller() {
+		return seller;
+	}
+
+	public void setSeller(SellerDTO seller) {
+		this.seller = seller;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SaleDTO other = (SaleDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
